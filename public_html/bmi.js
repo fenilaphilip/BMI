9ff30504887fd97428ab938b59e27b07e403cd23
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-10-08 12:34:09 +0200

    code clean up

--- public_html/bmi.js
+++ public_html/bmi.js
@@ -1,14 +1,11 @@
-/* 
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
-
-function calculate(){
-    let weight= document.getElementById("wt").value ; /* kg */
-    let height= document.getElementById("ht").value; /* cm */
-    let bmi = ((weight/(height* height))*10000)
-    document.getElementById("bmi").innerHTML= "BMI =" + bmi;
+function calculate() {
+  let weight = document.getElementById("weight").value; /* kg */
+  let height = document.getElementById("height").value; /* cm */
+  let bmi = Math.round((weight / Math.pow(height, 2)) * 10000);
+  document.getElementById("bmi").innerHTML = "BMI = " + bmi;
 }
-
-
